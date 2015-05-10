dcbe0c9916f4373572a30cecda4fdfd8d4d263dd
cycorld <charleschoi87@gmail.com>
2015-05-10 17:42:06 +0900

    activeadmin

---
+++ youthcham/app/admin/notice.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Notice do
+  permit_params :title, :sub_title, :image_src, :body, :hits
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if resource.something?
+#   permitted
+# end
+
+
+end
--- youthcham/config/routes.rb
+++ youthcham/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   get 'notice/write'
 
   get 'notice/view'
